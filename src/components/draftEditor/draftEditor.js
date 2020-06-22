500ec537d38d849387ff66cbae252923f57b2b24
bangarangler <bangarangler@gmail.com>
2020-06-22 13:21:16 -0400

    refactored to make it more readable and take up less...
    jsx space.  added switch statment instead of a bunch of ifs.
    
    now mapping over to arrays to handle button display. to be cleaned up
    further but a good start.

--- src/components/draftEditor/draftEditor.js
+++ src/components/draftEditor/draftEditor.js
@@ -9,33 +9,19 @@ const DraftEditor = () => {
     EditorState.createEmpty()
   );
 
-  const changeStyle = (style) => {
-    update(RichUtils.toggleInlineStyle(editorState, `${style}`));
+  const handleKeyCommand = (command, editorState) => {
+    const newState = RichUtils.handleKeyCommand(editorState, command);
+
+    if (newState) {
+      console.log("newState :>> ", newState);
+      update(newState);
+      return "handled";
+    }
+    return "not-handled";
   };
 
-  const pluarisBlockQuoteStyle = (contentBlock) => {
-    const type = contentBlock.getType();
-    if (type === "blockquote") {
-      return `${styles.blockQuote}`;
-    }
-    if (type === "unordered-list-item") {
-      return `${styles.ulItem}`;
-    }
-    if (type === "ordered-list-item") {
-      return `${styles.olItem}`;
-    }
-    if (type === "code-block") {
-      return `${styles.codeBlock}`;
-    }
-    if (type === "header-one") {
-      return `${styles.headerOne}`;
-    }
-    if (type === "header-two") {
-      return `${styles.headerTwo}`;
-    }
-    if (type === "header-three") {
-      return `${styles.headerThree}`;
-    }
+  const changeStyle = (style) => {
+    update(RichUtils.toggleInlineStyle(editorState, `${style}`));
   };
 
   const onTab = (e) => {
@@ -48,6 +34,30 @@ const DraftEditor = () => {
   };
 
   const update = (editorState) => setEditorState(editorState);
+
+  const pluarisBlockQuoteStyle = (contentBlock) => {
+    const type = contentBlock.getType();
+    switch (type) {
+      case "blockquote":
+        return `${styles.blockQuote}`;
+      case "unordered-list-item":
+        return `${styles.ulItem}`;
+      case "ordered-list-item":
+        return `${styles.olItem}`;
+      case "code-block":
+        return `${styles.codeBlock}`;
+      case "header-one":
+        return `${styles.headerOne}`;
+      case "header-two":
+        return `${styles.headerTwo}`;
+      case "header-three":
+        return `${styles.headerThree}`;
+      case "blockquote":
+        return `${styles.blockQuote}`;
+      default:
+        return;
+    }
+  };
 
   const styleOptions = [
     {
@@ -103,57 +113,22 @@ const DraftEditor = () => {
     },
   ];
 
-  const handleKeyCommand = (command, editorState) => {
-    const newState = RichUtils.handleKeyCommand(editorState, command);
-
-    if (newState) {
-      console.log("newState :>> ", newState);
-      update(newState);
-      return "handled";
-    }
-    return "not-handled";
-  };
   return (
     <>
-      <button type="button" onClick={() => changeStyle("BOLD")}>
-        B
-      </button>
-      <button type="button" onClick={() => changeStyle("ITALIC")}>
-        I
-      </button>
-      <button type="button" onClick={() => changeStyle("UNDERLINE")}>
-        U
-      </button>
-      <button type="button" onClick={() => changeStyle("STRIKETHROUGH")}>
-        S
-      </button>
-      <button type="button" onClick={() => toggleBlockType("blockquote")}>
-        BlockQuote
-      </button>
-      <button
-        type="button"
-        onClick={() => toggleBlockType("unordered-list-item")}
-      >
-        UL
-      </button>
-      <button
-        type="button"
-        onClick={() => toggleBlockType("ordered-list-item")}
-      >
-        OL
-      </button>
-      <button type="button" onClick={() => toggleBlockType("code-block")}>
-        Code Block
-      </button>
-      <button type="button" onClick={() => toggleBlockType("header-one")}>
-        H1
-      </button>
-      <button type="button" onClick={() => toggleBlockType("header-two")}>
-        H2
-      </button>
-      <button type="button" onClick={() => toggleBlockType("header-three")}>
-        H3
-      </button>
+      {blockTypeOptions.map((option) => {
+        return (
+          <button type="button" onClick={() => toggleBlockType(option.style)}>
+            {option.label}
+          </button>
+        );
+      })}
+      {styleOptions.map((option) => {
+        return (
+          <button type="button" onClick={() => changeStyle(option.style)}>
+            {option.label}
+          </button>
+        );
+      })}
       <Editor
         editorState={editorState}
         onTab={onTab}
